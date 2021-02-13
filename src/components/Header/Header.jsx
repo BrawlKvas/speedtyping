daaf1e2d8dc129713030bd8837033af2142b1cfc
Pukki Konstantin <pukkikonstantin@gmail.com>
2021-02-13 20:10:30 +0300

    Навигация

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
-import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Button, makeStyles, Toolbar, Typography } from '@material-ui/core'
+import { NavLink } from 'react-router-dom'
 
 const useStyles = makeStyles((theme) => ({
   drawerHeader: {
@@ -14,9 +15,17 @@ const Header = () => {
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" noWrap>
+          <Typography variant="h6" noWrap style={{ flexGrow: 1 }}>
             Speedtyping
           </Typography>
+
+          <Button variant="outlined" color="inherit" style={{ marginRight: '15px' }}>
+            <NavLink to="/test">Тестирование</NavLink>
+          </Button>
+
+          <Button variant="outlined" color="inherit">
+            <NavLink to="/test/completed">Информация о результатах</NavLink>
+          </Button>
         </Toolbar>
       </AppBar>
 
